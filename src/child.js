75608ca5508d24fb457f0e4923762d21ca1b18a2
Suhaib Iftikhar <Suhaibkhan4@users.noreply.github.com>
2020-06-22 19:02:27 +0500

    Expenseapp

---
+++ src/child.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Child() {
+  return (
+    <div className="container"> 
+    <h1> Expense Tracker</h1>
+
+    <h3>Your Balance <br/> $260 </h3>
+    <div className
+    </div>
+
+  );
+}
+
+export default Child;
